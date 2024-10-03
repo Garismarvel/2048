cb73ad55af23b3061494e964e688c5709f1f80c8
GarisMarvel <78643557+Garismarvel@users.noreply.github.com>
2024-10-03 21:51:46 +0700

    Update main.js

--- main.js
+++ main.js
@@ -378,7 +378,7 @@ GameManager.prototype.move = function (direction) {
                     self.score += merged.value;
 
                     // The mighty 2048 tile
-                    if (merged.value === 2048) self.won = true;
+                    if (merged.value === 268435456) self.won = true;
                 } else {
                     self.moveTile(tile, positions.farthest);
                 }
